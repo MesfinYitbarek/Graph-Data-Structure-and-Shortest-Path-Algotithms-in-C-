6e9a7d5c8e890164736e5e8e78ed33502dbf463c
Kirubel Tesfaw <kirubeltesfaw@gmail.com>
2025-01-17 22:56:03 +0300

    Implement edge relaxation and negative-weight cycle detection

--- src/BellmanFord.cpp
+++ src/BellmanFord.cpp
@@ -3,9 +3,35 @@
 #include <climits>
 using namespace std;
 
-void bellmanFord(const Graph& graph, int startNode) {
+void bellmanFord(const Graph &graph, int startNode)
+{
     int numberOfNodes = graph.getAdjList().size();
     vector<int> distances(numberOfNodes, INT_MAX);
     distances[startNode] = 0;
 
+    for (int i = 1; i < numberOfNodes; ++i)
+    {
+        for (int u = 0; u < numberOfNodes; ++u)
+        {
+            for (const auto &[v, weight] : graph.getAdjList()[u])
+            {
+                if (distances[u] != INT_MAX && distances[u] + weight < distances[v])
+                {
+                    distances[v] = distances[u] + weight;
+                }
+            }
+        }
+    }
+
+    for (int u = 0; u < numberOfNodes; ++u)
+    {
+        for (const auto &[v, weight] : graph.getAdjList()[u])
+        {
+            if (distances[u] != INT_MAX && distances[u] + weight < distances[v])
+            {
+                cout << "Graph contains a negative-weight cycle.\n";
+                return;
+            }
+        }
+    }
 }
